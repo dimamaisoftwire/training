3751aec6f1e572b80850a2c73adf3a3b1af3378a
dimamaisoftwire <dima.mai@softwire.com>
2026-03-12 12:28:06 +0000

    added user command parsing file and json parser with strategy + factory pattern

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 from account import AccountManager
-from file_parser import CSVParser
+from file_parser import CSVParser, JSONParser
+from user_input import parse_input
+
 
 def main():
     accounts = AccountManager()
-    csv_parser = CSVParser()
-    transactions = csv_parser.parse_file("DodgyTransactions2015.csv")
-    accounts.process_transactions(transactions)
     parse_input(accounts)
-
-def parse_input(accounts):
-    print("Please enter: ")
-    print("list all - to list all transactions")
-    print("list bogus - to list bogus transactions")
-    print("list [name] - to list all transactions with this name")
-
-    user_input = input()
-    if user_input.lower() == "list all":
-        accounts.list_all()
-        return
-    if user_input.lower() == "list bogus":
-        accounts.list_bogus()
-        return
-    if user_input.lower()[:5] == "list ":
-        account = user_input[5:]
-        accounts.list_account(account)
-        return
-    print("Unknown command")
 
 if __name__ == '__main__':
     main()
